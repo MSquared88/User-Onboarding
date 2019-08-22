6913a70f711f4b8ed214680ce6b6733ec7fadaef
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-08-22 17:55:00 -0500

    made mvp

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { Form, Field, withFormik} from 'formik'
 import * as Yup from 'yup'
 
-const UserForm = ({errors, touched, values}) => {
+import User from './User'
+
+const UserForm = ({errors, touched, values, status}) => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    if (status) {
+      setUsers([...users, status]);
+    }
+    }, [status]);
   return (
     <div>
       <h1></h1>
@@ -28,6 +36,9 @@ const UserForm = ({errors, touched, values}) => {
         </label>
         <button>Submit</button>
       </Form>
+      {users.map(user => (
+        <User  name={user.name} email={user.email} password={user.password}/>
+      ))}
     </div>
   );
 }
